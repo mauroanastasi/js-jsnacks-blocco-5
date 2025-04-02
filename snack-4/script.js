6c7a002fd78fdb35d97ec7685778e3cee2d5e97b
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-02 15:47:11 +0200

    [UPD] snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const name = posts.map((element)=>{
+  return element.author
+})
+
+console.log(name)
